be3cef0af25fe2937a09fbd8fb0e1d9d96be7ccc
Rmcgirt <127910622+Rmcgirt@users.noreply.github.com>
2023-03-27 22:46:36 -0400

    completed script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,42 @@
-// TODO: Declare any global variables we need
-var numHeads
+var numHeads = 0;
+var numTails = 0;
+
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    console.log('its working')
+    document.getElementById("flip").addEventListener('click', () => {
+        var randomNumber = Math.round(Math.random())
+        if(randomNumber == 0){
+            numHeads = numHeads + 1;
+            document.getElementById("penny-image").setAttribute("src", "assets/images/penny-heads.jpg")
+            document.getElementById("message").innerText = "You Flipped Heads!"
+            document.getElementById("heads").innerText = numHeads;
+            
 
+        }
+        else{
+            numTails += 1;
+            document.getElementById("penny-image").setAttribute("src", "assets/images/penny-tails.jpg")
+            document.getElementById("message").innerText = "You Flipped Tails!"
+            document.getElementById("tails").innerText = numTails;
+            
+        }
+        document.getElementById("heads-percent").innerText = Math.round((numHeads/(numHeads+numTails))*100)+"%"
+        document.getElementById("tails-percent").innerText = Math.round((numTails/(numHeads+numTails))*100)+"%"
+    })
+    document.getElementById("clear").addEventListener('click', () => {
+        numHeads = 0
+        numTails = 0 
+        document.getElementById("message").innerText = "Let's Get Rolling!"
+        document.getElementById("heads").innerText = numHeads; 
+        document.getElementById("tails").innerText = numTails;
+        document.getElementById("heads-percent").innerText = "0%"
+        document.getElementById("tails-percent").innerText = "0%"
+
+
+    })
     // TODO: Add event listener and handler for flip and clear buttons
 
     // Flip Button Click Handler
